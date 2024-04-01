0f92e95928be51122c453f003ebb4a52a52873e8
Alan Strassmeier <102873586+alan-strassmeier@users.noreply.github.com>
2024-04-01 19:56:58 +0200

    publicando jogo adivinhe o numero

---
+++ app/validação.js
@@ -0,0 +1,57 @@
+var body = document.body;
+function verificarValorChute(chute) {
+  const numero = +chute;
+
+  if (chuteInvalido(numero)) {
+    if (chute.toUpperCase() === "GAME OVER") {
+      document.body.innerHTML = `
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `;
+      document.body.style.backgroundColor = "black";
+    } else {
+      elementoChute.innerHTML += "<div>Valor inválido</div>";
+      return;
+    }
+  }
+  if (numeroMaiorOuMenor(numero)) {
+    elementoChute.innerHTML += `
+    <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+    `;
+    return;
+  }
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+    `;
+  } else if (numero > numeroSecreto) {
+    body.classList.remove("quente");
+    body.classList.add("frio");
+    elementoChute.innerHTML += `
+    <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+    `;
+  } else {
+    body.classList.remove("frio");
+    body.classList.add("quente");
+    elementoChute.innerHTML += `
+    <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+    `;
+  }
+}
+
+function chuteInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenor(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar-novamente") {
+    window.location.reload();
+  }
+});
